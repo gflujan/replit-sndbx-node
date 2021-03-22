8ed31d8c11faa86bb871c1e921e783e75b54c803
Metal Gabe <gfl.wrk@gmail.com>
2021-03-22 23:23:31 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const lodash = require('lodash'); 
+const moment = require('moment'); 
+
+console.log('==================================');
+console.log();
+/* Above is a space separator to give my reading some "breathing room" */
+
+console.log('bueller?');
+
+/* Below is a space separator to give my reading some "breathing room" */
+console.log();
+console.log('==================================');
+
+// more 
