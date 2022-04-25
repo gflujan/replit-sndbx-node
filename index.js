1a5f846330f74295ce200e6f628d5c6cf9c197bc
Gabriel F. Lujan <gfl.wrk@gmail.com>
2022-04-25 22:28:07 +0000

    updated NPM packages

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-const lodash = require('lodash'); 
-const moment = require('moment'); 
+// const Database = require("@replit/database"); 
+// const { DateTime } = require('luxon');
+// const Day = require('dayjs'); 
+// const lodash = require('lodash'); 
+// const { createServer } = require('http');
+// const WebSocket = require('ws');
+
+// DB Ops 
+// const db = new Database(); 
+
+// db.set("prefix", "bueller").then(() => {}); 
+
+// db.get("prefix").then(value => {
+//   console.debug('🚀--BLLR?: db_get -> value ->', value);
+// }); 
+
+// db.delete("key").then(resp => {
+//   console.debug('🚀--BLLR?: db_delete -> response ->', resp);
+// }); 
+
+// db.list().then(keys => {
+//   console.debug('🚀--BLLR?: db_list -> keys ->', keys);
+// }); 
+
+// db.list("prefix").then(matches => {
+//   console.debug('🚀--BLLR?: db_list_w_arg -> matches ->', matches);
+// }); 
 
 console.log('==================================');
 console.log();
 /* Above is a space separator to give my reading some "breathing room" */
 
 console.log('bueller?');
+
+// const d = DateTime.fromObject({ hour: 10, minute: 0, second: 0 }).setZone('America/Los_Angeles');
+// console.log(d);
 
 /* Below is a space separator to give my reading some "breathing room" */
 console.log();
